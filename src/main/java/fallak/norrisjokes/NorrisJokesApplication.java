435335ab07f192ba158fb858252eef73a737f15b
fallak <sh.fallak@gmail.com>
2020-06-11 15:19:31 +0200

    Switched off java based configuration to xml configuration. close# 3

--- src/main/java/fallak/norrisjokes/NorrisJokesApplication.java
+++ src/main/java/fallak/norrisjokes/NorrisJokesApplication.java
@@ -2,8 +2,10 @@ package fallak.norrisjokes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class NorrisJokesApplication {
 
 	public static void main(String[] args) {
